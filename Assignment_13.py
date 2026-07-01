eb2a7e33624cb8bc43403d9f525248d8127fed42
Mansi Ingale <ingalemansi70@gmail.com>
2026-07-01 06:32:23 +0530

    Q1: Basic Array Creation

---
+++ Assignment_13.py
@@ -0,0 +1,15 @@
+#Question 1
+import numpy as np
+# a)
+arr_1d = np.array([10,20,30,40,50])
+# b) 
+arr_2d = np.array([[1,2,3],
+                  [4,5,6],
+                  [7,8,9]])
+print("One dimensional Array : ",arr_1d) 
+print("Two dimensional Array(3*3) : ",arr_2d)
+
+
+
+
+
