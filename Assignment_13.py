f300761a87a4f770700ea1e8b545ccaefb21da8d
Mansi Ingale <ingalemansi70@gmail.com>
2026-07-01 07:01:40 +0530

    Question3 -> using np.arange

--- Assignment_13.py
+++ Assignment_13.py
@@ -17,8 +17,20 @@ print("1 Dimensional array with 8 zeros : ",zero_arr)
 #b)
 twoD_arr1 = np.ones((4,4))
 print("4*4 array with 1 is : ",twoD_arr1)
+#c)
 twoD_arr2 = np.zeros((3,3))
 print("3*3 array with 0 is : ",twoD_arr2)
+
+# Question 3)
+#a)
+arr = np.arange(0,20)
+print("Array with Numbers from 0 to 20 (step 1) : ",arr)
+#b)
+even_arr = np.arange(10,51,2)
+print("Array of Even numbers from 10 to 50 :",even_arr)
+#c)
+new_arr = np.arange(5,100,5)
+print("Array of Numbers from 5 to 100 with step of 5 : ",new_arr)
 
 
 
