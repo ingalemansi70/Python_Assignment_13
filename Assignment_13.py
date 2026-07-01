be514e1e8789bca4e9110cc51e8cb4284da85634
Mansi Ingale <ingalemansi70@gmail.com>
2026-07-01 06:36:11 +0530

    Question 2 np.zeros() & np.ones()

--- Assignment_13.py
+++ Assignment_13.py
@@ -10,6 +10,17 @@ print("One dimensional Array : ",arr_1d)
 print("Two dimensional Array(3*3) : ",arr_2d)
 
 
+#Question 2)
+#a)
+zero_arr = np.zeros(8)
+print("1 Dimensional array with 8 zeros : ",zero_arr)
+#b)
+twoD_arr1 = np.ones((4,4))
+print("4*4 array with 1 is : ",twoD_arr1)
+twoD_arr2 = np.zeros((3,3))
+print("3*3 array with 0 is : ",twoD_arr2)
+
+
 
 
 
