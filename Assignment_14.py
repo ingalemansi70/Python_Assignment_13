6dd6ad68ac6b113a5992e964a446c0c6526fce3f
Mansi Ingale <ingalemansi70@gmail.com>
2026-07-03 04:17:06 +0530

    Question 2: Array Properties

---
+++ Assignment_14.py
@@ -0,0 +1,7 @@
+import numpy as np
+#Question 1)
+arr_2D = np.random.randint(10,100,(5,6))
+print("2D Array of shape (5,6) with random integer values : \n",arr_2D)
+print("Shape of above array is : ",arr_2D.shape)
+print("Total number of elements in the array : ",arr_2D.size)
+print("Data type of the array : ",arr_2D.dtype)
